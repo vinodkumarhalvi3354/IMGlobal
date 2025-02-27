bfa72daa3befd84e09132d8fef4f32d36ad4c9db
Halvivinodkumar <155708634+Halvivinodkumar@users.noreply.github.com>
2025-02-27 11:02:25 +0530

    final commit1

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,13 +7,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigModule
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Make the config globally available
+      isGlobal: true, 
     }),
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
